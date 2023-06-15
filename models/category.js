9e57c47a564c62a2d056f436774c6aa877753685
AipuAmeh <aipuameh.16@gmail.com>
2023-06-15 07:03:17 -0400

    updated gitignore, added env files, changed starter code

--- models/category.js
+++ models/category.js
@@ -1,29 +1,20 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 
-class Location extends Model {}
+const sequelize = require('../config/connection.js');
 
-Location.init(
+class Category extends Model {}
+
+Category.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    },
+    // define columns
+  },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'location',
+    modelName: 'category',
   }
 );
 
-module.exports = Location;
-
+module.exports = Category;
--- models/product.js
+++ models/product.js
@@ -1,30 +1,23 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
 const sequelize = require('../config/connection');
-class Traveller extends Model {}
-Traveller.init(
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    },
+    // define columns
+  },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'traveller',
+    modelName: 'product',
   }
 );
-module.exports = Traveller
 
+module.exports = Product;
