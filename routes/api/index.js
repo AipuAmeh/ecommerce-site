37d6efa9949f9bea66e74581bdebe7fae46267a9
AipuAmeh <aipuameh.16@gmail.com>
2023-06-14 21:41:42 -0400

    Addded all starter code

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const tripRoutes = require('./tripRoutes');
+const travelerRoutes = require('./travelerRoutes');
+const locationRoutes = require('./locationRoutes');
+
