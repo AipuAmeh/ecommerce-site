05aa6bebd9e138016ac344100da2fb2df57f95dc
AipuAmeh <aipuameh.16@gmail.com>
2023-06-14 21:49:33 -0400

    changed file names to homework

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,2 @@
-const router = require('express').Router();
-const tripRoutes = require('./tripRoutes');
-const travelerRoutes = require('./travelerRoutes');
-const locationRoutes = require('./locationRoutes');
+
 
