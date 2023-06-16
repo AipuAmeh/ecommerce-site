34060abf0af5a034b6a18a31782169e9dfe26ce0
AipuAmeh <aipuameh.16@gmail.com>
2023-06-15 21:40:51 -0400

    changed category model name

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import models
 const Product = require('./Product');
-const Category = require('./Categories')
+const Category = require('./Category')
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
